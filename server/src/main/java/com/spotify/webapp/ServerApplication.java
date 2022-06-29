51640f46c99642eb6db453dd55f067273e135f85
Swarup <swaroop4202@gmail.com>
2022-06-29 22:34:29 +0530

    Initial Commit

---
+++ server/src/main/java/com/spotify/webapp/ServerApplication.java
@@ -0,0 +1,13 @@
+package com.spotify.webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerApplication.class, args);
+	}
+
+}
